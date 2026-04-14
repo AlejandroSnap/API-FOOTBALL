f78eaa15332bd3535369ed0a829df58051495bed
Alejandro <alejandrosnap31@gmail.com>
2026-04-14 15:00:16 -0500

    new

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,7 +1,11 @@
 import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
 # client = MongoClient("mongodb://user:admin@localhost:27017/?authSource=admin")
+
+load_dotenv()
+
 client = MongoClient(os.getenv("MONGO_URI"))
 
 db = client["main"]
